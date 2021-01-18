22c9d2307494f704ef2aa10fe0f3b1c2588bdee4
Rtheodor <rafaeldacunhatheodoro@gmail.com>
2021-01-17 23:43:10 -0300

    Operadores de atribuição

---
+++ op_atribuicao.js
@@ -0,0 +1,22 @@
+var valorUm = 8;
+var valorDois = 3;
+
+//valorUm += valorDois;
+//valorUm = valorUm + valroDois;
+//document.write("Operador de atribuição com soma: " + valorUm + "<br>");
+
+//valorUm -=valorDois;
+//valorUm = valorUm - valorDois;
+//document.write("OPerador de atribuição com subtração: " +  valorUm + "<br>");
+
+//valorUm *=valorDois;
+//valorUm = valorUm * valorDois;
+//document.write("Operador de atribuição  com multiplicacão: " + valorUm + "<br>");
+
+//valorUm /=valorDois;
+//valorUm = valorUm / valorDois;
+//document.write("Operador de atribuição  com divisão: " + valorUm + "<br>");
+
+valorUm %=valorDois;
+//valorUm = valorUm % valorDois;
+document.write("Operador de atribuição  com módulo: " + valorUm + "<br>");
