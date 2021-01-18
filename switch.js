bf524352f48ea7de68febef2e55fd15cb65fdaf8
Rtheodor <rafaeldacunhatheodoro@gmail.com>
2021-01-18 15:46:22 -0300

    Como usar switch no javascript

---
+++ switch.js
@@ -0,0 +1,24 @@
+var tipo = 2;
+
+switch(tipo){
+    case 1:
+        document.write(" Apartamento ");
+        break;
+    
+    case 2:
+            document.write(" Casa ");
+            break;
+
+    case 3:
+            document.write(" Comercial ");
+            break;
+
+    case 4:
+             document.write(" Sobrado ");
+            break;    
+    
+    default:
+        document.write("Não corresponde a nenhum imovel ");
+   
+
+};
